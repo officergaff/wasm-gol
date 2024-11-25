9a6c1cbcc01718546cc840ab2ee90a27921caa37
Li Xin Rui <li.xinrui3@gmail.com>
2024-11-25 14:59:50 -0500

    added fps

--- www/index.js
+++ www/index.js
@@ -67,17 +67,6 @@ const drawCells = () => {
   ctx.stroke();
 };
 
-let animationId = null;
-
-const renderLoop = () => {
-  debugger;
-  universe.tick();
-  drawGrid();
-  drawCells();
-
-  animationId = requestAnimationFrame(renderLoop);
-};
-
 const isPaused = () => {
   return animationId === null;
 };
@@ -118,6 +107,56 @@ canvas.addEventListener('click', (event) => {
   drawGrid();
   drawCells();
 });
+
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById('fps');
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+  render() {
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+         Frames per Second:
+                  latest = ${Math.round(fps)}
+                  avg of last 100 = ${Math.round(mean)}
+                  min of last 100 = ${Math.round(min)}
+                  max of last 100 = ${Math.round(max)}
+                  `.trim();
+  }
+})();
+
+let animationId = null;
+
+const renderLoop = () => {
+  debugger;
+  fps.render();
+  universe.tick();
+  drawGrid();
+  drawCells();
+
+  animationId = requestAnimationFrame(renderLoop);
+};
 
 drawGrid();
 drawCells();
